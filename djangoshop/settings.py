8e6bdc507acda1b15ca2cb629dfa764cf5561253
JeeHyungPark <jhp0227@naver.com>
2020-02-08 01:38:30 +0900

    base.html 템플릿 적용

--- djangoshop/settings.py
+++ djangoshop/settings.py
@@ -124,5 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'products')
 MEDIA_URL = '/products/'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
